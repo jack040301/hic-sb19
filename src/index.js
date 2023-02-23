719f0943e7a08e05001229c581ab2767db4ba2a5
Jacqueline Porral <jackdaisuki04@gmail.com>
2023-02-23 20:48:44 +0800

    HICProject

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import './style.css'
+import Profile from './views/profile'
+import Home from './views/home'
+import BlogPost from './views/blog-post'
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Route component={Profile} exact path="/profile" />
+        <Route component={Home} exact path="/" />
+        <Route component={BlogPost} exact path="/blog-post" />
+      </div>
+    </Router>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
